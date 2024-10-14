b493fbca6dd18a907957928675c16fcb66fe10ca
sparrow212 <taranik187@gmail.com>
2024-10-15 02:22:19 +0300

    Add files via upload

---
+++ exampl 2.py
@@ -0,0 +1,15 @@
+# "1 st program"
+print((9**0.5)*5)
+# "2 nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+# "3 rd program"
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2) == (2*(2+2)))
+# "4 th program"
+print('123.456')
+float('123.456')
+print(123.456*10)
+print(1234.56%10)
+int(4.5599999999999)
+print(int((123.456)*10)%10)
